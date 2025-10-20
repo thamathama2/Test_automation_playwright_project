a2ee05b01ab624929a53e1e64ff7614aa892531f
Thamathama Simethi <tsimethi@shoprite.co.za>
2025-10-20 06:21:00 +0200

    readme fix

--- playwright.config.js
+++ playwright.config.js
@@ -35,9 +35,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
-
+  
   /* Configure projects for major browsers */
   projects: [
     {
